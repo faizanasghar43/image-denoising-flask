2a4ded4767338ffeb592900886148597bb0f5c4b
Muhammad Faizan Asghar <bsce19021@itu.edu.pk>
2023-11-30 19:29:04 +0500

    initial commit

---
+++ denoising.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.image as mpimg
+
+
+def denoiser(file_path):
+    slash_index = file_path.rfind('/')
+
+    if slash_index == -1:
+        file_full_name = file_path
+    else:
+        file_extra_path = file_path[:slash_index]
+        file_full_name = file_path[slash_index + 1:]
+
+    dot_index = file_full_name.rfind('.')
+    file_name = file_full_name[:dot_index]
+    file_extension = file_full_name[dot_index + 1:]
+
+    input_img = mpimg.imread(file_path)
+
+    output_img = cv2.fastNlMeansDenoisingColored(input_img, None, 10, 10, 7, 21)
+
+    output_file_name = file_name + "_denoised"
+    output_file_full_name = output_file_name + '.' + file_extension
+    output_file_path = output_file_full_name if slash_index == -1 else file_extra_path + '/' + output_file_full_name
+
+    plt_file_name = file_name + "_plt"
+    plt_file_full_name = plt_file_name + '.' + file_extension
+    plt_file_path = plt_file_full_name if slash_index == -1 else file_extra_path + '/' + plt_file_full_name
+
+    mpimg.imsave(output_file_path, output_img)
